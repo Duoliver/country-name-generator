a642e3b99cf3c30a6782aa9679eefe5b4e395b48
Honeysalad <oliveira.douglasedu@outlook.com>
2019-04-09 22:22:32 -0300

    Early syllable generation with three letters and name saving

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
+//Variables (letters and suffix arrays)
 var letters = [["a", "e", "i", "o", "u"],
  ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]];
- var suffix = ["land", "ia", "istan", "mark"];
+var suffix = ["land", "ia", "istan", "mark"];
 
- 
+//Functions
 function randomic(max){
     return Math.floor((Math.random()*max));
 }
@@ -46,11 +47,16 @@ function twoSyllable(name){
     }
 }
 
+function threeSyllable(name){ //expand cases as developing
+    return newConsonant()+newVowel(name)+newConsonant();
+}
+
 function generateName(){
+    console.log("GenerateName: New name generation starting");
     var name = "";
     var syllables = [];
     for(var j = 0; j <= randomic(4)+2; j++){
-        syllables[j] = randomic(2)+1; //INCREASE as developing
+        syllables[j] = randomic(3)+1; //INCREASE as developing
         switch(syllables[j]){
             case 1:
                 name += oneSyllable(name);
@@ -58,14 +64,17 @@ function generateName(){
                 break;
             case 2:
                 name += twoSyllable(name);
-                console.log("TwoSyllable: ("+name.substring(0, name.length-2)+")"+name.substring(name.length-2));
+                console.log("TwoSyllable: ("+name.substring(0, name.length-3)+")"+name.substring(name.length-2));
                 break;
+            case 3:
+                name += threeSyllable(name);
+                console.log("ThreeSyllable: ("+name.substring(0, name.length-4)+")"+name.substring(name.length-2));
             default:
                 break;
         }
         //name+="-";
     }
-    if(randomic(2) == 1){
+    if(randomic(2) == 1 || syllables.length == 1){
         var selectedSuffix = suffix[randomic(suffix.length)];
         console.log("Suffix: generated suffix for "+name);
         console.log("Suffix: "+selectedSuffix);
@@ -81,38 +90,28 @@ function generateName(){
 }
 
 function listNames(limit){
-    var list = ""
+    var list = []
+    document.getElementById("list_generated").innerHTML = "";
     for(var i = 0; i < limit; i++){
-        list += "<li>"+generateName()+"</li> ";
+        name = generateName();
+        document.getElementById("list_generated").innerHTML += '<li class="country">'+name+'</li>';
+        list[i] = name;
     }
-    document.getElementById("lista").innerHTML = list;
+    return list;
 }
-/*
-$("#generate").click(function(){
-    
-});
-*/
+
+//JQuery
 $(function(){
-    listNames(10);
+    var list = listNames(10);
     $("#generate").click(function(){
-        listNames(10);
+        list = listNames(10);
+        console.log(list);
     });
+    $(".country").click(function(){ // does not work properly
+        console.log("SaveName: Going to save the name");
+        var index = $(".country").index(this);
+        console.log("SaveName: Name saved");
+        document.getElementById("list_saved").innerHTML += '<li>'+list[index]+'</li>';
+
+    })
 });
-
-
-/*
-function generateName(){
-    for(var i = 0; i < 10; i++){ //generates ten words
-        var name = "";
-        for(var j = 0; j <= randomic(6); j++){ //number of syllables
-            for(var k = 0; k <= randomic(6); k++){ //letters (syllable size)
-                var selector = randomic(2);
-                name += letters[selector][selector == 0 ? randomic(5) : randomic(21)];
-        }
-        if(randomic(2) == 1){
-            name += suffix[randomic(suffix.length)];
-        }
-        return format(name);
-    }
-}
-*/
